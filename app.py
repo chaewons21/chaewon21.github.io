7580b86da9e790d9c4b606cb28fcb6e5ce6be265
chaewons21 <136100411+chaewons21@users.noreply.github.com>
2023-06-13 08:56:31 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+# app.py
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+from scipy import signal
+
+import streamlit as st
+
+def main():
+    st.title('Step and Bode Plot')
+    
+    # 전달함수 계수
+    num = [100]
+    den = [1, 5, 106]
+
+    # 시스템 응답
+    t, y = signal.step(signal.TransferFunction(num, den))
+
+    # 응답곡선 그리기
+    fig1 = plt.figure()
+    plt.plot(t, y)
+    plt.xlabel('Time')
+    plt.ylabel('Output')
+    plt.title('Step Response')
+    plt.grid(True)
+    st.pyplot(fig1)
+    
+    # 주파수 응답
+    w, mag, phase = signal.bode(signal.TransferFunction(num, den))
+
+    # Bode plot 그리기
+    fig2 = plt.figure()
+    plt.semilogx(w, mag)
+    plt.ylabel('Gain (dB)')
+    plt.title('Bode Plot (Gain)')
+    plt.grid(True)
+    st.pyplot(fig2)
+    
+    fig3 = plt.figure()
+    plt.semilogx(w, phase)
+    plt.xlabel('Frequency (rad/s)')
+    plt.ylabel('Phase (degrees)')
+    plt.title('Bode Plot (Phase)')
+    plt.grid(True)
+    st.pyplot(fig3)
+
+if __name__ == '__main__':
+    main()
